a163f17cb7147a27cf8b9a899821df88b5c5aa10
s194261 <s194261@student.pg.edu.pl>
2024-12-06 00:35:00 +0100

    Model6.12.py

--- projekt.py
+++ projekt.py
@@ -1,1 +1,25 @@
-#TEST TEST TEST
+import sympy as sy
+
+#Wszystkie zmienne dane które podawane będą z klawiatury:
+#Temperatura ciała i temperatura wody (początkowe)
+Tc, Tw = sy.symbols('Tc Tw')
+#masa ciała i objętość wody
+m, V = sy.symbols('m V')
+#cieplo wlasciwe ciala
+Cc = sy.symbols('Cc')
+
+#stałe wartosci:
+#gestosc wody
+rho = 1 #kg/m^3
+#cieplo wlasciwe wody
+Cw = 4200 #J/kgK
+
+if Tw > Tc:
+    dQ = (Tc - Tw)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
+    To = dQ/(rho*V*Cw)+Tw
+    dS = dQ*(Tc-Tw)/(Tc*Tw)
+
+if Tw < Tc:
+    dQ = (Tw - Tc)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
+    To = (-dQ)/(rho*V*Cw)+Tw
+    dS = dQ*(Tw-Tc)/(Tc*Tw)
